8cea3e7aca2b4b92e6093721fd74d7e3f33228f2
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-21 02:29:17 +0530

    todo from custom handlers

--- main.go
+++ main.go
@@ -8,12 +8,18 @@ import (
 
 const PORT = ":8080"
 
+func healthHandler(w http.ResponseWriter, h *http.Request) {
+	w.WriteHeader()
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", http.FileServer(http.Dir(".")))
 	mux.Handle("/assets", http.StripPrefix("/", http.FileServer(http.Dir("./assets/logo.png"))))
 
+	//health checkpoint
+	mux.HandleFunc("/healthz", healthHandler)
 	server := &http.Server{Handler: mux, Addr: PORT}
 
 	fmt.Printf("server started at http://localhost:%s\n", PORT)
